dec8307a769fbacd5f855282c0dc29187e40119f
Edd Barrett <vext01@gmail.com>
2023-11-06 14:42:49 +0000

    test again.

--- .buildbot.sh
+++ .buildbot.sh
@@ -2,10 +2,17 @@
 
 set -e
 
+# We only want checks to really run when a github merge queue pushes a branch.
+if [ "${EVENT}" = "pull_request" ]; then
+    exit 0
+elif [ "${EVENT}" != "push" ]; then
+    echo "Unknown event. Failing CI"
+fi
+
 for i in `seq 30`; do
     echo $i
 done
 
 echo hi 1
 
-exit 0
+exit 1
